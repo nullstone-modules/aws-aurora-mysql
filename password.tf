5de257b556cb18501caa9ad2e125aa3face637c0
Brad Sickles <brad.sickles@gmail.com>
2025-05-29 17:13:56 -0400

    Eliminated problematic special characters from password generation.

--- password.tf
+++ password.tf
@@ -1,12 +1,32 @@
-// TODO: We should move to IAM Authentication instead of creating a password
-
 resource "random_password" "this" {
   // Master password length constraints differ for each database engine. For more information, see the available settings when creating each DB instance.
   length  = 16
   special = true
 
   // The password for the master database user can include any printable ASCII character except /, ", @, or a space.
-  override_special = "!#$%&*()-_=+[]{}<>:?"
+  // We're also excluding the following characters:
+  // ':' - not allowed by DMS (Database Migration Service)
+  // ';' - not allowed by DMS
+  // '+' - not allowed by DMS
+  // '%' - not allowed by DMS, confuses url encoding
+  // '?' - confuses url encoding
+  // '#' - confuses url encoding
+  // '[' - confuses url encoding
+  // ']' - confuses url encoding
+  // '{' - confuses url encoding
+  // '}' - confuses url encoding
+  // '(' - issues with batch files
+  // ')' - issues with batch files
+  // '&' - issues with batch files
+  // '!' - issues with batch files
+  // '^' - issues with batch files
+  // '<' - issues with batch files
+  // '>' - issues with batch files
+  override_special = "$*-_="
+
+  lifecycle {
+    ignore_changes = [override_special] // Prevent changing passwords for provisioned dbs
+  }
 }
 
 resource "aws_secretsmanager_secret" "password" {
